62daa45c2f5b9b133634dbb404b8a05eae1479db
meg <zimnickimegan@yahoo.com>
2024-09-30 15:10:29 -0400

    changed

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,3 +1,4 @@
+const { GraphQLError } = require('graphql');
 const jwt = require('jsonwebtoken');
 
 // set token secret and expiration date
@@ -5,18 +6,26 @@ const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-  // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
+  // We will use GraphQL to return an Authentication error if encountered
+  AuthenticationError: new GraphQLError('Could not authenticate user.', {
+    extensions: {
+      code: 'UNAUTENTICATED'
+    }
+  }),
+  // function for our authenticated routes 
+    // No longer using HTTPS routes so can remove (res, next)
+  authMiddleware: function ({ req }) {
     // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+      // Added functionality to allow tokens to be sent via req.body
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
+      token = token.split(' ').pop().trim(); // Splits the token string into an array to return the actual token
     }
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      return req;
     }
 
     // verify token and get user data out of it
@@ -25,11 +34,10 @@ module.exports = {
       req.user = data;
     } catch {
       console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
     }
 
-    // send to next endpoint
-    next();
+    // Return the request object so it can be passed to the resolver as 'context'
+    return req;
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
