6c723d01409116f328ddbb6b0b001eb670eeeac5
meg <zimnickimegan@yahoo.com>
2024-10-01 15:55:46 -0400

    new instance apollo and graphQL

--- server/server.js
+++ server/server.js
@@ -1,50 +1,50 @@
 const express = require('express');
-// Import ApolloServer and Apollo-specific express middleware
-const { ApolloServer } = require('@apollo/server');
-const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
 const db = require('./config/connection');
-// Import typeDefs and resolvers once defined
-const { typeDefs, resolvers } = require('./schemas')
-// Import the authentication middleware
-const { authMiddleware } = require('./utils/auth');
+// const routes = require('./routes');
+const { ApolloServer } = require('apollo-server-express'); // import ApolloServer
+const { typeDefs, resolvers } = require('./schemas'); // import typeDefs and resolvers
+const { authMiddleware } = require('./utils/auth'); // import authMiddleware
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-//const routes = require('./routes'); // Replacing with GraphQL
 
+// Create an instance of Apollo Server
 const server = new ApolloServer({
-  typeDefs,
-  resolvers
+  typeDefs,    // pass in the typeDefs and resolvers from the schemas and resolvers files for the server to use
+  resolvers,
+  context: authMiddleware,
+  formatError(err) {
+    console.log(err);
+    return err;
+  }
 });
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async () => {
-  await server.start();
+  await server.start(); // start the server
+  server.applyMiddleware({ app }); // apply the server to the Express app as middleware
 
-  app.use(express.urlencoded({ extended: true }));
-  app.use(express.json());
-
-  app.use('/graphql', expressMiddleware(server, {
-    context: authMiddleware
-  }));
+  app.use(express.urlencoded({ extended: true })); // extended: true allows for nested objects in query strings
+  app.use(express.json()); // tells the server that we are using JSON
 
   // if we're in production, serve client/build as static assets
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist'))); // Changed from build to dist
+    app.use(express.static(path.join(__dirname, '../client/dist')));
   
-    // Wildcard route to redirect to index.html
     app.get('*', (req, res) => {
       res.sendFile(path.join(__dirname, '../client/dist/index.html'));
     });
   }
 
+
+  // app.use(routes);
+
   db.once('open', () => {
-    app.listen(PORT, () => {
-      console.log(`API server running on port ${PORT}!`);
-      console.log(`Use GraphQL at https://localhost:${PORT}/graphql`);
-    });
+    app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
   });
 };
 
+// Call the async function to start the server
 startApolloServer();
