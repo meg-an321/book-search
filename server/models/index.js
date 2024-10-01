30a01ba16f67976062f5a9d97e385ac237959f5f
meg <zimnickimegan@yahoo.com>
2024-10-01 16:10:36 -0400

    imported .book

--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,4 @@
 const User = require('./User');
+const Book = require('./Book'); //importing the Book model 
 
-module.exports = { User };
+module.exports = { User, Book }; //exporting the User and Book models which will be used in the server.js file
