55822532b242e850c5e1b6aa73e6fc0848b7ce17
meg <zimnickimegan@yahoo.com>
2024-10-01 16:27:30 -0400

    switched api to graphql

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     open: true,
     proxy: {
-      '/api': {
+      '/graphql': { //switched from api to graphql as requested by the server
         target: 'http://localhost:3001',
         secure: false,
         changeOrigin: true
